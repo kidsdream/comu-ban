684ff54722a859b8bfc59d0c33c5bd23d189bfb7
dream <docomo905dream@yahoo.co.jp>
2020-01-04 23:50:55 +0900

    CSS basic layout

--- index.php
+++ index.php
@@ -102,6 +102,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9b759cafa6.js" crossorigin="anonymous"></script>
 
     <!-- JavaScript -->
     <!-- jQuery -->
@@ -120,86 +121,93 @@
   <body>
 
   <header id="l-header">
-    <div class="l-container">
-      <img src="img/logo.png" alt="コミュばん！のロゴ" class="u-mt10">
+    <div class="p-title">
+      <h1>COMUBAN</h1>
     </div>
   </header>
 
-  <aside class="l-aside">
-    <div class="">
-      <a href="#"></a>
-    </div>
-  </aside>
-
-  <main>
-    <!-- メッセージが正常に投稿された場合の表示 -->
-    <?php if (!empty($success_message)): ?>
-      <div class="c-info">
-        <p class="c-info__message c-successMessage"><?php echo $success_message; ?></p>
-      </div>
-    <?php endif; ?>
-
-    <!-- エラー表示 -->
-    <?php if (!empty($errors)): ?>
-      <div class="c-info">
-        <ul class="error_message">
-          <?php foreach ($errors as $value): ?>
-            <li><?php echo $value; ?></li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-    <?php endif; ?>
-
-    <form action="index.php" method="post" class="c-form">
-      <ul>
-        <li>
-          <label for="name">名前</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value="<?php if(isset($name)) { print($name); } ?>"
-          />
-        </li>
-          <li>
-            <label for="message">内容</label>
-            <textarea name="message" id="message" ><?php if(isset($message)) { print($message); } ?></textarea>
-          </li>
-        <li><input type="submit" name="submit" class="c-btn" value="書き込む" /></li>
+  <div class="l-wrapper">
+    <aside class="l-aside">
+      <ul class="c-listAside">
+        <li><a href="#"><i class="fas fa-caret-square-right"></i>アニメ</a></li>
+        <li><a href="#"><i class="fas fa-gamepad"></i>ゲーム</a></li>
+        <li><a href="#"><i class="fas fa-music"></i>　音楽</a></li>
+        <li><a href="#"><i class="fas fa-tv"></i>テレビ</a></li>
+        <li><a href="#"><i class="fas fa-broadcast-tower"></i>ラジオ</a></li>
       </ul>
-    </form>
+    </aside>
 
-    <?php
-      $dbh = db_connect();
-      $sql = 'SELECT id, name, message, created_at FROM messages ORDER BY id DESC';
-      $stmt = $dbh->prepare($sql);
-      $stmt->execute();
-      $dbh = null;
-    ?>
+    <main id="l-main">
+      <!-- メッセージが正常に投稿された場合の表示 -->
+      <?php if (!empty($success_message)): ?>
+        <div class="c-info">
+          <p class="c-info__message c-successMessage"><?php echo $success_message; ?></p>
+        </div>
+      <?php endif; ?>
 
-    <div class="l-flex">
-      <?php while($value = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-        <article class="p-contents">
-          <div class="p-info">
-            <h2 class="p-info__name"><?php echo $value['name']; ?></h2>
-            <time class="p-info__time"><?php echo date('Y年m月d日 H:i', strtotime($value['created_at'])); ?></time>
-          </div>
-          <p class="p-message"><?php echo $value['message']?></p>
-        </article>
-      <?php endwhile ?>
-    </div>
-  </main>
+      <!-- エラー表示 -->
+      <?php if (!empty($errors)): ?>
+        <div class="c-info">
+          <ul class="error_message">
+            <?php foreach ($errors as $value): ?>
+              <li><?php echo $value; ?></li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+      <?php endif; ?>
 
-  <footer id="l-footer">
-    <p>
-      <small>&copy; 2019 - <span id="thisYear"></span>  DREAM</small>
-      <script type="text/javascript">
-        date = new Date();
-        thisYear = date.getFullYear();
-        document.getElementById("thisYear").innerHTML = thisYear;
-      </script>
-    </p>
-  </footer>
+      <form action="index.php" method="post" class="c-form">
+        <ul>
+          <li>
+            <label for="name">名前</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value="<?php if(isset($name)) { print($name); } ?>"
+            />
+          </li>
+            <li>
+              <label for="message">内容</label>
+              <textarea name="message" id="message" ><?php if(isset($message)) { print($message); } ?></textarea>
+            </li>
+          <li><input type="submit" name="submit" class="c-btn" value="書き込む" /></li>
+        </ul>
+      </form>
+
+      <?php
+        $dbh = db_connect();
+        $sql = 'SELECT id, name, message, created_at FROM messages ORDER BY id DESC';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $dbh = null;
+      ?>
+
+      <div class="l-flex">
+        <?php while($value = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+          <article class="p-contents">
+            <div class="p-info">
+              <h2 class="p-info__name"><?php echo $value['name']; ?></h2>
+              <time class="p-info__time"><?php echo date('Y年m月d日 H:i', strtotime($value['created_at'])); ?></time>
+            </div>
+            <p class="p-message"><?php echo $value['message']?></p>
+          </article>
+        <?php endwhile ?>
+      </div>
+    </main>
+
+    <footer id="l-footer">
+      <p>
+        <small>&copy; 2019 - <span id="thisYear"></span>  DREAM</small>
+        <script type="text/javascript">
+          date = new Date();
+          thisYear = date.getFullYear();
+          document.getElementById("thisYear").innerHTML = thisYear;
+        </script>
+      </p>
+    </footer>
+
+  </div>
 
   </body>
 </html>
